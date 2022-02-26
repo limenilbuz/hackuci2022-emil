203b8b9da1da0dfaecb3df63be44f33059351332
Emil Zublingit config  --global user.name Emil <ejzublin@gmail.com>
2022-02-26 11:05:50 -0800

    somethin

--- app.js
+++ app.js
@@ -30,35 +30,29 @@ function renderMachines(doc) {
     // add event listener to the add buttons so that they have functionality
     add_button.addEventListener('click', (evnt) => {
         evnt.stopPropagation(); // stops the default action
-        let id = evnt.target.parentElement.getAttribute('data-id'); // gets the FIREBASE id of the machine that is clicked
+
+        // gets the FIREBASE id of the machine that is clicked
+        let id = evnt.target.parentElement.getAttribute('data-id'); 
         
-        var new_size;
-        //console.log(db.collection('machines').doc(id).increment(1));
+        // store the specific machine from FIREBASE
         const machine_to_be_updated = db.collection('machines').doc(id);
 
+        // async function to incremement the queue size
         machine_to_be_updated.get().then((doc)=>{
             new_size = doc.data().queue_size + 1;
-            
+
             db.collection('machines').doc(id).update({
                 name: doc.data().name,
                 queue_size: new_size
             }); // updates the data stored in the FIREBASE database
         });
 
-        console.log(doc.data());
+        //console.log(doc.data());
         
         
     }) 
 }
 
-/*
-db.collection('machines').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        renderMachines(doc);
-    })
-})
-<<<<<<< HEAD
-*/
 
 // real time listener
 
@@ -66,15 +60,19 @@ db.collection('machines').orderBy('name', 'asc').onSnapshot(snapshot => {
     let changes = snapshot.docChanges();
     changes.forEach(change => {
         if(change.type == "added"){
+            // renders all the machines intially
             renderMachines(change.doc);
         }
         else if (change.type == "modified"){
             // selects the list element
             let li = machineList.querySelector('[data-id=' + change.doc.id + ']');
+            // childNodes[1] represents the queue_size span element
+            // this code updates that span element to reflect the new queue size
             li.childNodes[1].innerHTML = change.doc.data().queue_size;
             
         }
         else if (change.type == "removed"){
+            // probably wont be used...
             // store the li element according to that data id that is removed
             let li = machineList.querySelector('[data-id=' + change.doc.id + ']');
             machineList.removeChild(li);
